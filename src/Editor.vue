c5685503beaf0395e53af9237c63d2b8e554e529
songjein <jeinsong100@gmail.com>
2018-05-27 13:12:49 +0000

    new editor ui

--- src/Editor.vue
+++ src/Editor.vue
@@ -3,56 +3,87 @@
 	ref. https://serversideup.net/preview-file-uploads-with-axios-and-vuejs/
 span-->
 <template>
-	<div class="container">
-		<h1>업로드 기능 개편 테스트(개발중이어서 갑자기 안될 수도)</h1>
-		<ul>
-			<li>여러 이미지 업로드 (빠른 이어붙이기 가능)</li>
-			<li>선택된 이미지 제거</li>
-			<li>메인 이미지 선택 (이미지 클릭시 메인 이미지 선정)</li>
-			<li>이미지 프리뷰</li>
-			<li>정렬 기능</li>
-			<li>캡션 기능</li>
-		</ul>
-		<h3 @click="check" style="cursor:pointer">→ 현재 목록 체크 (F12 console 로 확인)</h3>
-		<h3>→ 업로드 확인하러 가기 (아직)</h3>
+	<div id="root">
+		<!-- ( -->
+		<div class="container">
+			<h1>업로드 기능 개편 테스트(개발중이어서 갑자기 안될 수도)</h1>
+			<ul>
+				<li>여러 이미지 업로드 (빠른 이어붙이기 가능)</li>
+				<li>선택된 이미지 제거</li>
+				<li>메인 이미지 선택 (이미지 클릭시 메인 이미지 선정)</li>
+				<li>이미지 프리뷰</li>
+				<li>정렬 기능</li>
+				<li>캡션 기능</li>
+			</ul>
+			<h3 @click="check" style="cursor:pointer">→ 현재 목록 체크 (F12 console 로 확인)</h3>
+			<h3>→ 업로드 확인하러 가기 (아직)</h3>
 
-		<input type="file" id="files" ref="files" accept="image/*" multiple @change="handleFilesUpload()"/>
-		<div style="height: 10px"></div>
+			<!-- ( -->
+			<div id="editor-head">
+				<input type="text" id="title" placeholder="이 곳에 제목을 입력해주세요." autocomplete="off">
+				
+				<div style="height: 50px;"></div>
+				<hr>
+				<div style="height: 30px;"></div>
 
-		<button id="add-files" @click="addFiles()">사진 추가</button>
-
-		<div style="height: 20px"></div>
-
-		<div v-show="files.length > 0"><b>사진 목록</b></div>
-
-		<div style="height: 20px"></div>
-
-		<div id="preview-container">
-			<div v-for="(file, key) in files" class="file-listing">
-				<img class="preview selected" v-if="main_img_idx == key" v-bind:ref="'image' + parseInt( key )" @click="select(key)"/>
-				<img class="preview" v-if="main_img_idx != key" v-bind:ref="'image' + parseInt( key )" @click="select(key)"/>
-				<br>
-				{{ file.name }} 
-				<div style="color:gray; cursor:pointer;">
-					캡션(미완성): {{ captions[key] }}
-					<input type="text" v-model="captions[key]"/>
+				<div id="editor-menu">
+					<div id="add-img" class="editor-menu-item">
+						<input type="file" id="files" ref="files" accept="image/*" multiple @change="handleFilesUpload()"/>
+						<span @click="addFiles()">+ 이미지 추가</span>
+					</div>
+					<div class="editor-menu-item" v-if="!isSecret" @click="toggleSecret">전체공개</div>
+					<div class="editor-menu-item" v-if="isSecret"  @click="toggleSecret"><b>비공개</b></div>
+					<div class="editor-menu-item">바인더선택</div>
+					<div class="editor-menu-item">저작권 라이센스</div>
 				</div>
-				<span class="mv" @click="mvUp(key)"><b>위로!</b></span>
-				<span class="mv" @click="mvDown(key)"><b>아래로!!</b></span>
-				<span class="remove-file" @click="removeFile(key)">(제거)</span>
-				<div style="height: 10px"></div>
-			</div>	
+
+				<div style="height: 30px;"></div>
+			</div> 
+			<!-- ) -->
 		</div>
+		<!-- ) -->
+		<!-- ( -->
+		<div class="bottom-container">
+			<!-- ( -->
+			<div id="editor-bottom">
+				<div id="previews">
+					
+					<div v-for="(file, key) in files" class="file-listing">
+						<!-- ( -->
+						<div class="preview-container">
+							<img class="preview selected" v-if="main_img_idx == key" v-bind:ref="'image' + parseInt( key )" @click="select(key)"/>
+							<img class="preview" v-if="main_img_idx != key" v-bind:ref="'image' + parseInt( key )" @click="select(key)"/>
+							<i class="fas fa-arrow-circle-up mv mvUp" @click="mvUp(key)"></i>
+							<i class="fas fa-arrow-circle-down mv mvDown" @click="mvDown(key)"></i>
+							<i class="fas fa-times-circle remove" @click="removeFile(key)"></i>
+						</div>
+						<!-- ) -->
+						<div style="color:gray; cursor:pointer;">
+							캡션(미완성): {{ captions[key] }}
+							<input type="text" v-model="captions[key]"/>
+						</div>
+						<div style="height: 10px"></div>
+					</div>	
+				</div>
 
-		<div style="height: 20px"></div>
+				<div style="height: 20px"></div>
 
-		<button id="submit" @click="submitFiles()">업로드</button>
+				<textarea placeholder="이 곳에 내용을 입력해 주세요."></textarea>
+
+				<button id="submit" @click="submitFiles()">Finish</button>
+
+				<div style="height: 50px;"></div>
+			</div> 
+			<!-- ) -->
+		</div>
+		<!-- ) -->
 	</div>
 </template>
 <script>
 export default {
   data () {
     return {
+			isSecret: false,
 			files: [],
 			captions: [],
 			main_img_idx: 0,
@@ -62,6 +93,9 @@ export default {
   components: {
   },
   methods: {
+		toggleSecret() {
+			this.isSecret = !this.isSecret;
+		},
 		init() {
 			this.files =  [];
 			this.captions = [];
@@ -126,6 +160,11 @@ export default {
 		removeFile(key) {
 			this.files.splice(key, 1);	
 			this.captions.splice(key, 1);	
+			
+			console.log("remove Result");
+			console.log(this.files);
+			console.log(this.captions);
+
 			this.getImagePreviews();
 		},
 		select(key) {
@@ -157,15 +196,21 @@ export default {
 </script>
 <style scoped>
 	.container {
+		background: white;	
 		position: relative;	
 	}
+	.bottom-container {
+		background: rgb(253,253,253);	
+		position: relative;	
+	}
+
 	span.remove-file {
 		color: red;	
 		cursor: pointer;
 		font-weight: bold;
 	}
 	div.file-listing img{
-		max-width: 30%;
+		width: 95%;
 		cursor: pointer;
 	}
 	input[type=file]{
@@ -173,19 +218,114 @@ export default {
 		opacity: 0;
 		top: -1000px;
 	}
-	#add-files {
-		font-size: 2em;	
-	}	
 	#submit {
-		font-size: 2em;	
+		font-size: 1.5em;	
+		width: 100%;
+    background-color: transparent; /* Green */
+    border: 1px solid rgb(103,179,177);
+		color: rgb(103,179,177);
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+		cursor: pointer;
 	}
 	.selected {
-		border: 7px solid rgb(43,244,226);	
+    border: 7px solid rgb(103,179,177);
 	}
+
 	.mv {
-		text-decoration: underline;	
-		font-size: 1.5em;
+		font-size: 2.5em;
 		cursor: pointer;
-		color: green;
+    color: rgb(103,179,177);
+	}
+	.mvUp {
+		position: absolute;
+		top: 100px;
+		left: -70px;
+	}
+	.mvDown {
+		position: absolute;
+		bottom: 100px;
+		left: -70px;
+	}
+	.remove {
+		position: absolute;
+		top: 20px;
+		right: 70px;
+		font-size: 2.5em;
+		cursor: pointer;
+    color: white; 
+	}
+
+	.preview-container {
+		position: relative;	
+	}
+
+	#title {
+		background: transparent;
+		border: none;
+		font-size: 2em;
+		width: 800px;
+	}
+	#editor-head {
+		width: 60%;
+		margin: auto;
+		background: white;
+	}
+
+	#editor-bottom{
+		width: 60%;
+		margin: auto;
+		background: rgb(253,253,253);
+	}
+
+	hr {
+		display: block;
+		height: 1px;
+		border: 0;
+		border-top: 1px solid #ccc;
+		margin: 1em 0;
+		padding: 0; 
+	}
+
+	textarea {
+		width: 90%;	
+		border: none;
+		font-size: 1.1em;
+		min-height: 300px;
+		resize: none;
+		background: rgb(253,253,253);
+	}
+
+	textarea:focus, input:focus {
+		outline-width: 0;	
+	}
+
+	#editor-menu {
+		width: 100%;
+		height: 50px;
+	}
+	.editor-menu-item {
+		display: inline-block;
+		width: 24%;	
+		text-align: center;
+		color: rgb(150,150,150);
+		cursor: pointer;
+	}
+	#add-img {
+		color: rgb(103,179,177);
+	}
+	::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+		color: rgb(150,150,150);
+		opacity: 1; /* Firefox */
+	}
+
+	:-ms-input-placeholder { /* Internet Explorer 10-11 */
+		color: rgb(150,150,150);
+	}
+
+	::-ms-input-placeholder { /* Microsoft Edge */
+		color: rgb(150,150,150);
 	}
 </style>
